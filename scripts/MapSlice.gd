e840864ce9004180d5bf71d5dd2415bf7a6cb770
CyanideGourmet <57600898+CyanideGourmet@users.noreply.github.com>
2019-11-12 06:13:48 +0100

    Player mechanics and map looping
    Gracz może teraz poruszać się na boki, oraz przyspieszać lub zwalniać. Kamera podąża za graczem pionowo, ale pozwala mu sięswobodnie poruszać poziomo. Mapa składająca się z dwóch wycinków (niebieskiego oraz różowego) zapętla się, pozwalając na nieskończony lot.

---
+++ scripts/MapSlice.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export var level_size = 6895
+
+func _on_MapSlice_body_exited(body):
+	if body == get_tree().get_root().get_node("Main").find_node("Player"):
+		position.y -= 2 * level_size
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,29 @@
-extends Area2D
+extends KinematicBody2D
 
 export var movement_speed = 400
-export var forward_speed = 5
-var screen_size
+export var forward_speed = 400
+export var max_forward_speed = 800
+export var min_forward_speed = 150
+export var acceleration_speed = 20
+var camera_pos
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	camera_pos = $Camera.position
 
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+	if Input.is_action_pressed("ui_up") and forward_speed < max_forward_speed:
+		forward_speed += acceleration_speed
+	if Input.is_action_pressed("ui_down") and forward_speed > min_forward_speed:
+		forward_speed -= acceleration_speed
 	velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized()
 		velocity.x *= movement_speed
 		velocity.y *= forward_speed
 	position += velocity * delta
+	position.x = clamp(position.x, camera_pos.x, camera_pos.x + 1280)
