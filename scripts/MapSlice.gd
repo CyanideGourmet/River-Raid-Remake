91b89d5ce55fe3b7a6f50a89deeb7d1e5ea6c50e
CyanideGourmet <57600898+CyanideGourmet@users.noreply.github.com>
2019-12-03 21:23:34 +0100

    Map generation optimisation
    Mapa jest generowana sprawniej minimalizując opóźnienia w grze. Dodane generowanie prostych wysp.
    
    Ta wersja jest bez wypełniania terenu.

--- scripts/MapSlice.gd
+++ scripts/MapSlice.gd
@@ -1,23 +1,26 @@
 extends TileMap
 
 export var level_size = 8704
-var temp = []
-var left
-var right
-var middle
 var map_array = []
-var end_n = 0
+var start_end_width = [0, 0]
+var start_end_height = [20, 252]
+var max_width = 0
+var copy_offset
+var step_memory = []
+
+func _ready():
+	$Area.connect("body_exited", self, "_on_MapSlice_body_exited")
+	_generate()
+
+func _on_MapSlice_body_exited(body):
+	yield(get_tree().create_timer(1), "timeout")
+	if body == get_parent().find_node("Player"):
+		position.y -= 2 * level_size
+		_generate()
 
 func _random_int(x, y):
 	randomize()
 	return randi()%(y+x+1)+x
-
-func _zero_temp():
-	temp = []
-	for i in range(20):
-		temp.append([])
-		for j in range(272):
-			temp[i].append([0, 0, 0])
 
 func _choose_dir(last, n):
 	var x
@@ -31,185 +34,108 @@ func _choose_dir(last, n):
 	elif last[0] == 2:
 		x = _random_int(0, 1)
 		x *= 2
-	if n > 18 and x == 1:
+	if n > 25 and x == 1:
 		x = 0
 	if n < 2 and x == 2:
 		x = 0
+	step_memory.append(x)
 	return x
 
-func _ready():
-	$Area.connect("body_exited", self, "_on_MapSlice_body_exited")
+func _generate():
+	step_memory = []
+	max_width = 0
+	map_array = []
 	for i in range(60):
 		map_array.append([])
 		for j in range(272):
 			map_array[i].append([0, 0, 0])
-	_generate()
-
-func _on_MapSlice_body_exited(body):
-	yield(get_tree().create_timer(1), "timeout")
-	if body == get_parent().find_node("Player"):
-		position.y -= 2 * level_size
-		_generate()
-
-func _generate():
-	_temp()
-	_left()
-	_right()
-	_middle()
-	_merge()
+	_template()
+	_right_side_copy()
+	_island()
 	_place_tiles()
+    #PRINT
+	for i in range(272):
+		var a = []
+		for j in range(60):
+			a.append(map_array[j][i][0])
+		#print(a)
+	print("\n\n")
 
-func _merge():
-	for i in range(20):
-		for j in range(272):
-			map_array[i][j] = left[i][j]
-			map_array[i+40][j] = right[i][j]
-
-func _temp():
-	_zero_temp()
+func _template():
 	var last = [0, 0]
-	var n = 9
+	var n = _random_int(5, 12)
 	var m = 20
 	var x
-	temp[n][m][0] = 3
+	start_end_width[0] = n
+	max_width = n
+	map_array[n][m][0] = 3
 	while(m <= 252):
 		x = _choose_dir(last, n)
 		if x == 0:
-			temp[n][m][1] = 1
+			map_array[n][m][1] = 1
 			m += 1
-			temp[n][m][0] = 3
+			map_array[n][m][0] = 3
 			last[1] = last[0]
 			last[0] = 0
 		elif x == 1:
-			temp[n][m][1] = 2
+			map_array[n][m][1] = 2
 			n += 1
-			temp[n][m][0] = 4
+			map_array[n][m][0] = 4
 			last[1] = last[0]
 			last[0] = 1
 		elif x == 2:
-			temp[n][m][1] = 4
+			map_array[n][m][1] = 4
 			n -= 1
-			temp[n][m][0] = 2
+			map_array[n][m][0] = 2
 			last[1] = last[0]
 			last[0] = 2
-	temp[n][m][1] = 1
-	end_n = n
+		max_width = max(max_width, n)
+	map_array[n][m][1] = 1
+	start_end_width[1] = n
+	copy_offset = 59-max_width
 
-func _left():
-	left = temp.duplicate(true)
-	#UP
-	var n = 9
-	var m = 19
-	var x = 1
-	left[n][m][1] = 1
-	for i in range(11):
-		left[n][m][0] = 3
-		m -= 1
-		left[n][m][1] = 1
-	while(m > 0):
-		if x == 1:
-			left[n][m][0] = 2
-			n += 1
-			left[n][m][1] = 4
-			x = 0
-		elif x == 0:
-			left[n][m][0] = 3
-			m -= 1
-			left[n][m][1] = 1
-			x = 1
-	left[n][m][0] = 3
-	#DOWN
-	n = end_n
-	m = 253
-	x = 1
-	left[n][m][1] = 1
-	for i in range((272-252)-(20-n)-1):
-		left[n][m][0] = 3
-		m += 1
-		left[n][m][1] = 1
-	left[n][m][0] = 3
-	while(m < 271):
-		if x == 1:
-			left[n][m][1] = 2
-			n += 1
-			left[n][m][0] = 4
-			x = 0
-		elif x == 0:
-			left[n][m][1] = 1
+func _randomize_param():
+	var is_island = _random_int(0, 1)
+	var pass_width = 0
+	var island_start = 0
+	var island_end = 0
+	if is_island:
+		pass_width = 8
+		island_start = _random_int(39, 79)
+		island_end = _random_int(120, 160)
+	return [pass_width, island_start, island_end]
+
+func _right_side_copy():
+	var n = start_end_width[0]
+	var m = start_end_height[0]
+	for i in step_memory:
+		map_array[n+copy_offset][m] = map_array[n][m].duplicate()
+		map_array[n+copy_offset][m][2] = 1
+		if i == 0:
 			m += 1
-			left[n][m][0] = 3
-			x = 1
-	left[n][m][1] = 1
-	#FILL
-	n = 0
-	m = 0
-	for i in range(272):
-		while(left[n][m] == [0, 0, 0]):
-			left[n][m] = [1, 1, 0]
+		elif i == 1:
 			n += 1
-		n = 0
-		m += 1
+		elif i == 2:
+			n -= 1
+	map_array[n+copy_offset][m] = map_array[n][m]
+	map_array[n+copy_offset][m][2] = 1
 
-func _right():
-	right = temp.duplicate(true)
-	#UP
-	var n = 9
-	var m = 19
-	var x = 1
-	right[n][m][1] = 1
-	for i in range(12):
-		right[n][m][0] = 3
-		m -= 1
-		right[n][m][1] = 1
-	while(m > 0):
-		if x == 1:
-			right[n][m][0] = 4
-			n -= 1
-			right[n][m][1] = 2
-			x = 0
-		elif x == 0:
-			right[n][m][0] = 3
-			m -= 1
-			right[n][m][1] = 1
-			x = 1
-	right[n][m][0] = 3
-	#DOWN
-	n = end_n
-	m = 253
-	x = 1
-	right[n][m][1] = 1
-	for i in range((272-252)-n-1):
-		right[n][m][0] = 3
-		m += 1
-		right[n][m][1] = 1
-	right[n][m][0] = 3
-	while(m < 271):
-		if x == 1:
-			right[n][m][1] = 4
-			n -= 1
-			right[n][m][0] = 2
-			x = 0
-		elif x == 0:
-			right[n][m][1] = 1
-			m += 1
-			right[n][m][0] = 3
-			x = 1
-	right[n][m][1] = 1
-	#FILL
-	n = 19
-	m = 0
-	for i in range(272):
-		while(right[n][m] == [0, 0, 0]):
-			right[n][m] = [1, 1, 0]
-			n -= 1
-		while n >= 0 and right[n][m] != [0, 0, 0]:
-			right[n][m][2] = 1
-			n -= 1
-		n = 19
-		m += 1
+func _island():
+	var island_params = _randomize_param()
+	if island_params[0]:
+		var n = start_end_width[0]
+		var m = start_end_height[0] 
+		for i in step_memory:
+			if m >= island_params[1] and m <= island_params[2]:
+				map_array[n+island_params[0]][m] = map_array[n+copy_offset][m].duplicate()
+				map_array[n+copy_offset-island_params[0]][m] = map_array[n][m].duplicate()
+			if i == 0:
+				m += 1
+			elif i == 1:
+				n += 1
+			elif i == 2:
+				n -= 1
 
-func _middle():
-	pass
 
 func _place_tiles():
 	var x
@@ -217,30 +143,30 @@ func _place_tiles():
 		for j in range(272):
 			x = map_array[i][j]
 			if x == [2, 1, 1]:
-				set_cell(i, j, 4)
+				$TileMap.set_cell(i, j, 4)
 			elif x == [2, 1, 0]:
-				set_cell(i, j, 3, true)
+				$TileMap.set_cell(i, j, 3, true)
 			elif x == [4, 1, 1]:
-				set_cell(i, j, 3)
+				$TileMap.set_cell(i, j, 3)
 			elif x == [4, 1, 0]:
-				set_cell(i, j, 4, true)
+				$TileMap.set_cell(i, j, 4, true)
 			elif x == [3, 1, 1]:
-				set_cell(i, j, 2, false, false, true)
+				$TileMap.set_cell(i, j, 2, false, false, true)
 			elif x == [3, 1, 0]:
-				set_cell(i, j, 2, true, false, true)
+				$TileMap.set_cell(i, j, 2, true, false, true)
 			elif x == [2, 4, 0] or x == [4, 2, 1]:
-				set_cell(i, j, 2, false, true)
+				$TileMap.set_cell(i, j, 2, false, true)
 			elif x == [2, 4, 1] or x == [4, 2, 0]:
-				set_cell(i, j, 2)
+				$TileMap.set_cell(i, j, 2)
 			elif x == [3, 4, 0]:
-				set_cell(i, j, 4, true, true)
+				$TileMap.set_cell(i, j, 4, true, true)
 			elif x == [3, 4, 1]:
-				set_cell(i, j, 3, false, true)
+				$TileMap.set_cell(i, j, 3, false, true)
 			elif x == [3, 2, 0]:
-				set_cell(i, j, 3, true, true)
+				$TileMap.set_cell(i, j, 3, true, true)
 			elif x == [3, 2, 1]:
-				set_cell(i, j, 4, false, true)
+				$TileMap.set_cell(i, j, 4, false, true)
 			elif x == [1, 1, 0]:
-				set_cell(i, j, 1)
+				$TileMap.set_cell(i, j, 1)
 			else:
-				set_cell(i, j, 0)
+				$TileMap.set_cell(i, j, -1)
