6a6e152a56ae9e225c16e9f1456be02881d90707
Anti-GitHub-KJ <57634685+Anti-GitHub-KJ@users.noreply.github.com>
2019-12-21 14:16:52 +0100

    copy master

---
+++ README.md
@@ -0,0 +1,3 @@
+# River-Raid-Remake
+Remake of a classic '80s game River Raid, developed by a group of 4 aspiring gamedevs from a polish gamedev school located in Warsaw. The main and only purpose of this project is learning the process of creating a game.
+A 2 month-long project, based around remaking a game already made by others.
---
+++ scripts/Bullet.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export var bullet_distance = 800
+export var velocity = 1000
+
+var starting_position
+signal bullet_freed
+
+func _ready():
+	set_collision_mask_bit(3, 1)
+	set_collision_mask_bit(4, 1)
+	set_collision_mask_bit(5, 1)
+	set_collision_layer_bit(-4, 1)
+	starting_position = position
+
+func _process(delta):
+	if abs(position.y) - abs(starting_position.y) > bullet_distance:
+		_death()
+
+func _physics_process(delta):
+	var collision = move_and_collide(Vector2(0, -velocity) * delta)
+	if collision:
+		if collision.collider.is_in_group("enemy"):
+			collision.collider._death()
+		_death()
+
+func _death():
+	emit_signal("bullet_freed")
+	queue_free()
